a76383482eeb9f58a58ad805d8cab59b3f75fb0b
MinexAutomation <minex.automation@gmail.com>
2019-06-02 22:11:25 -0400

    Added ProcessRunner and ProcessRunOptions classes.

---
+++ source/R5T.NetStandard.OS/Code/Classes/ProcessRunOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace R5T.NetStandard.OS
+{
+    public class ProcessRunOptions
+    {
+        public string Command { get; set; }
+        public string Arguments { get; set; }
+        public DataReceivedEventHandler ReceiveOutputData { get; set; }
+        public DataReceivedEventHandler ReceiveErrorData { get; set; }
+    }
+}
---
+++ source/R5T.NetStandard.OS/Code/Classes/ProcessRunner.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace R5T.NetStandard.OS
+{
+    public static class ProcessRunner
+    {
+        public static string GetCommandLine(string command, string arguments)
+        {
+            var commandLine = $"{command} {arguments}";
+            return commandLine;
+        }
+
+        public static int Run(string command, string arguments)
+        {
+            var output = ProcessRunner.Run(command, arguments, Console.Out);
+            return output;
+        }
+
+        public static int Run(string command, string arguments, TextWriter outputWriter)
+        {
+            bool errorOccurred = false;
+            var runOptions = new ProcessRunOptions
+            {
+                Command = command,
+                Arguments = arguments,
+                ReceiveOutputData = (sender, e) =>
+                {
+                    outputWriter.WriteLine(e.Data);
+                },
+                ReceiveErrorData = (sender, e) =>
+                {
+                    if (e.Data != null)
+                    {
+                        outputWriter.WriteLine(e.Data);
+
+                        errorOccurred = true;
+                    }
+                }
+            };
+
+            int returnValue = ProcessRunner.Run(runOptions);
+
+            outputWriter.Flush();
+
+            if (errorOccurred)
+            {
+                outputWriter.WriteLine();
+
+                var commandLine = ProcessRunner.GetCommandLine(runOptions.Command, runOptions.Arguments);
+                outputWriter.WriteLine($"Command:\n{commandLine}");
+
+                throw new Exception("Process run error occurred.");
+            }
+
+            return returnValue;
+        }
+
+        public static int Run(ProcessRunOptions runOptions)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(runOptions.Command, runOptions.Arguments)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.OutputDataReceived += runOptions.ReceiveOutputData;
+            process.ErrorDataReceived += runOptions.ReceiveErrorData;
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            process.OutputDataReceived -= runOptions.ReceiveOutputData;
+            process.ErrorDataReceived -= runOptions.ReceiveErrorData;
+
+            int exitCode = process.ExitCode; // For debugging.
+
+            process.Close();
+
+            return exitCode;
+        }
+    }
+}
