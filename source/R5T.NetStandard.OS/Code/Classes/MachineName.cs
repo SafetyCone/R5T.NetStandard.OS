c5b1c0b4ce53417daad31d4ae6c783201931f7d0
MinexAutomation <minex.automation@gmail.com>
2019-06-02 22:31:01 -0400

    Added MachineName class.

---
+++ source/R5T.NetStandard.OS/Code/Classes/MachineName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.OS
+{
+    public class MachineName : TypedString
+    {
+        public MachineName(string value)
+            : base(value)
+        {
+        }
+    }
+}
