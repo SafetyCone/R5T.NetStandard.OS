a3073ca493ff9c49ecd65745e8f85af7ca79b920
MinexAutomation <minex.automation@gmail.com>
2019-06-02 22:45:13 -0400

    Added MachineName.GetExecuting() to get the System.Environment.MachineName value.

---
+++ source/R5T.NetStandard.OS.Construction/Code/Construction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.OS.Construction
+{
+    public static class Construction
+    {
+        public static void SubMain()
+        {
+            Construction.GetCurrentMachineName();
+        }
+
+        public static void GetCurrentMachineName()
+        {
+            Console.WriteLine($"{nameof(Environment)}.{nameof(MachineName)}: {Environment.MachineName}");
+        }
+    }
+}
--- source/R5T.NetStandard.OS.Construction/Code/Program.cs
+++ source/R5T.NetStandard.OS.Construction/Code/Program.cs
@@ -6,7 +6,7 @@ namespace R5T.NetStandard.OS.Construction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Construction.SubMain();   
         }
     }
 }
