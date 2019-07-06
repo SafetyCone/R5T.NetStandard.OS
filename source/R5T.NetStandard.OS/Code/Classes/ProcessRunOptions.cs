a8d64db4b0fdf42d403c9b29a3992a2823de65f7
MinexAutomation <minex.automation@gmail.com>
2019-07-06 14:37:50 -0400

    Added default ReceiveErrorData() method.

--- source/R5T.NetStandard.OS/Code/Classes/ProcessRunOptions.cs
+++ source/R5T.NetStandard.OS/Code/Classes/ProcessRunOptions.cs
@@ -6,6 +6,21 @@ namespace R5T.NetStandard.OS
 {
     public class ProcessRunOptions
     {
+        #region Static
+
+        public static void DefaultReceiveErrorData(object sender, DataReceivedEventArgs e)
+        {
+            if(e.Data is null)
+            {
+                return;
+            }
+
+            throw new Exception($"An error occurred: {e.Data}");
+        }
+
+        #endregion
+
+
         public string Command { get; set; }
         public string Arguments { get; set; }
         public DataReceivedEventHandler ReceiveOutputData { get; set; }
